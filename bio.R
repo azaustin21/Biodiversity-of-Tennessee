5d5d7fc23b175a11c77b791320f0b3c262815e74
azaustin21 <31972992+azaustin21@users.noreply.github.com>
2018-04-16 23:03:47 -0500

    Delete bio.R

--- bio.R
+++
@@ -1,6 +0,0 @@
-WCData <- read.csv("WatCon.csv", header = TRUE)
-WCData
-plot(WCData$POP, WCData$WC, xlab = "Population", ylab = "Water Consumption", pch = 16, cex = 1.3, col = "blue")
-
-WConLR <- lm(WC~POP,data=WCData)
-summary(WConLR)
